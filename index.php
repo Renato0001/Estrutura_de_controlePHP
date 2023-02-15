a353be52ed1a8e2f8294dcabac644aa289202956
Renato <boniex01@gmail.com>
2023-02-15 09:19:56 -0300

    new

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+    //condição verdadeira
+    if(5>2){
+        echo "Deu certo! entro no if <br>";
+    }
+    //condição falsa
+
+    if(5<2){
+        echo "Deu certo! entro no if <br>";
+    }
